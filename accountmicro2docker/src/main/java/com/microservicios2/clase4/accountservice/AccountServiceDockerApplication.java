51d88930a12102125056aacd60d5d8c740b05ef5
sbsromero <sbsromgmail.com>
2019-08-17 11:02:11 -0500

    microservicio desplegado en docker

---
+++ accountmicro2docker/src/main/java/com/microservicios2/clase4/accountservice/AccountServiceDockerApplication.java
@@ -0,0 +1,13 @@
+package com.microservicios2.clase4.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceDockerApplication.class, args);
+	}
+
+}
